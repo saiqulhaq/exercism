12c970c604c24b14e94f75676c2e838b34acaa9a
M Saiqul Haq <saiqulhaq@gmail.com>
2017-09-11 01:26:59 +0700

    Add RNA transcription problem

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,30 @@
+class Complement
+  attr_reader :dictionary
+  def initialize
+    @dictionary = {
+      'G' => 'C',
+      'C' => 'G',
+      'T' => 'A',
+      'A' => 'U'
+    }
+  end
+
+  def dna_to_rna(dna)
+    dictionary[dna.to_s.upcase]
+  end
+
+  def self.of_dna(dna)
+    instance = Complement.new
+
+    result = dna.to_s.chars.map do |char|
+      instance.dna_to_rna(char)
+    end.join('')
+
+    return '' if result.length != dna.to_s.length
+    result
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
